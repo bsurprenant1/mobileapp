c5b7357035494a7ffeb23deec4f53c8b16138f91
Brett <bsurprenant1@bose.com>
2015-07-09 09:52:19 -0400

    new

---
+++ mobileapp/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+from . import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.MobileHome_View.as_view(), name='mobile'),
+    url(r'^signIn/', views.MobileSignIn_View.as_view(), name='signin'),
+    url(r'^signOut/', views.MobileSignout_View_View.as_view(), name='signout'),
+    )
+
---
+++ mobileapp/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.views.generic import View
+from django.template import loader
+
+class MobileSignIn_View(View):
+    def get(self, request, *args, **kwargs):
+        t = loader.get_template('SignIn.html')
+
+        return HttpResponse(t.render())
+
+class MobileHome_View(View):
+    def get(self, request, *args, **kwargs):
+        t = loader.get_template('MobileHome.html')
+        return HttpResponse(t.render())
+
+class MobileSignout_View(View):
+    def get(self, request, *args, **kwargs):
+        t = loader.get_template('SignIn.html')
+        return HttpResponse(t.render())
+
+
